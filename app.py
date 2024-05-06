6e1657fe68ea8f1b50c8f91265a30e8042c49ea6
mringel-code <76100579+mringel-code@users.noreply.github.com>
2024-05-06 22:52:05 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
-import dash
-import dash_core_components as dcc
-import dash_html_components as html
-from dash.dependencies import Input, Output, State
+from dash import Dash, dcc, html, dash_table, Input, Output, State, callback
 
-app = dash.Dash(__name__)
+import base64
+import datetime
+import io
+
+import pandas as pd
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = Dash(__name__, external_stylesheets=external_stylesheets)
 
 app.layout = html.Div([
     dcc.Upload(
@@ -28,14 +33,44 @@ app.layout = html.Div([
     html.Div(id='output-data-upload'),
 ])
 
-
 def parse_contents(contents, filename, date):
+    content_type, content_string = contents.split(',')
+
+    decoded = base64.b64decode(content_string)
+    try:
+        if 'csv' in filename:
+            # Assume that the user uploaded a CSV file
+            df = pd.read_csv(
+                io.StringIO(decoded.decode('utf-8')))
+        elif 'xls' in filename:
+            # Assume that the user uploaded an excel file
+            df = pd.read_excel(io.BytesIO(decoded))
+    except Exception as e:
+        print(e)
+        return html.Div([
+            'There was an error processing this file.'
+        ])
+
     return html.Div([
         html.H5(filename),
+        html.H6(datetime.datetime.fromtimestamp(date)),
+
+        dash_table.DataTable(
+            df.to_dict('records'),
+            [{'name': i, 'id': i} for i in df.columns]
+        ),
+
+        html.Hr(),  # horizontal line
+
+        # For debugging, display the raw contents provided by the web browser
+        html.Div('Raw Content'),
+        html.Pre(contents[0:200] + '...', style={
+            'whiteSpace': 'pre-wrap',
+            'wordBreak': 'break-all'
+        })
     ])
 
-
-@app.callback(Output('output-data-upload', 'children'),
+@callback(Output('output-data-upload', 'children'),
               Input('upload-data', 'contents'),
               State('upload-data', 'filename'),
               State('upload-data', 'last_modified'))
@@ -46,6 +81,5 @@ def update_output(list_of_contents, list_of_names, list_of_dates):
             zip(list_of_contents, list_of_names, list_of_dates)]
         return children
 
-
 if __name__ == '__main__':
-    app.run_server(host='0.0.0.0',port=8080)
+    app.run(host='127.0.0.1',port=8050)
