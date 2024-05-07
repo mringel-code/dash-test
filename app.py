99de5e36038fbc4b1919cd4102c42cf537c605ce
Maik <mringel@deloitte.de>
2024-05-07 14:00:44 +0000

    update

--- app.py
+++ app.py
@@ -43,7 +43,6 @@ def parse_contents(contents, filename, date):
     content_type, content_string = contents.split(',')
 
     decoded = base64.b64decode(content_string)
-    predictor = retrieve_default(endpoint_name)
     try:
         #with open(filename, "wb") as fp:   # Unpacks the uploaded files
             #fp.write(decoded)   
@@ -62,40 +61,6 @@ def parse_contents(contents, filename, date):
                 html.H6(datetime.datetime.fromtimestamp(date)),
                 html.P(str(result))
             ])
-    '''
-    try:
-        if 'csv' in filename:
-            # Assume that the user uploaded a CSV file
-            df = pd.read_csv(
-                io.StringIO(decoded.decode('utf-8')))
-        elif 'xls' in filename:
-            # Assume that the user uploaded an excel file
-            df = pd.read_excel(io.BytesIO(decoded))
-    except Exception as e:
-        print(e)
-        return html.Div([
-            'There was an error processing this file.'
-        ])
-
-    return html.Div([
-        html.H5(filename),
-        html.H6(datetime.datetime.fromtimestamp(date)),
-
-        dash_table.DataTable(
-            df.to_dict('records'),
-            [{'name': i, 'id': i} for i in df.columns]
-        ),
-
-        html.Hr(),  # horizontal line
-
-        # For debugging, display the raw contents provided by the web browser
-        html.Div('Raw Content'),
-        html.Pre(contents[0:200] + '...', style={
-            'whiteSpace': 'pre-wrap',
-            'wordBreak': 'break-all'
-        })
-    ])
-'''
 
 @app.callback(Output('output-data-upload', 'children'),
               Input('upload-data', 'contents'),
@@ -140,6 +105,7 @@ def generate_summary(text):
             "stop": "<|eot_id|>"
         }
     }
+    predictor = retrieve_default(endpoint_name)
     response = predictor.predict(payload)
     return response["generated_text"]
 
