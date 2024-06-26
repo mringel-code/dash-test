12ebeab0c98b695440ee2d36ffb616319123ab8b
Maik <mringel@deloitte.de>
2024-06-26 11:31:41 +0000

    -

--- app.py
+++ app.py
@@ -103,9 +103,7 @@ def parse_contents(contents, filename, date):
     #loader = PyPDFLoader(BytesIO(decoded))
     #chunks = loader.load_and_split()
     chunks = reader.pages
-    
-    #print(f"Split document into {len(chunks)} chunks.")
-    #save_to_chroma(chunks)
+    save_to_chroma(chunks)
     #query_result = query_data("What is the deadline for the RfP?")
                 
     #except Exception as e:
@@ -183,7 +181,7 @@ def generate_summary(text):
     )
     return response['Body'].read().decode('utf-8')
 
-"""
+
 def save_to_chroma(chunks: List[Document]):
     # Clear out the database first.
     if os.path.exists(CHROMA_PATH):
@@ -193,12 +191,12 @@ def save_to_chroma(chunks: List[Document]):
     
     # Create a new DB from the documents.
     db = Chroma.from_documents(
-        chunks, lc_embed_model, persist_directory=CHROMA_PATH, collection_metadata=collection_metadata
+        chunks, embedding, persist_directory=CHROMA_PATH, collection_metadata=collection_metadata
     )
     db.persist()
     #print(f"Saved {len(chunks)} chunks to {CHROMA_PATH}.")
 
-
+"""
 def query_data(query_text):
     # Prepare the DB.
     embedding_function = lc_embed_model
